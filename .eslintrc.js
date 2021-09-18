f66f23ac39d2f3cc22caee183b142f2d8a5884c2
Johannes Werner <johannes.werner@hey.com>
2021-09-18 17:37:14 +0200

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   rules: {
